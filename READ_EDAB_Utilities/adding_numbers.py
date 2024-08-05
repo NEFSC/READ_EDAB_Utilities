a5eb27f0b7eabf395100f6309400b27a7fea5d38
LGruenburg <laura.gruenburg@noaa.gov>
2024-08-05 12:47:23 -0400

    creating pyproject.toml

---
+++ READ_EDAB_Utilities/adding_numbers.py
@@ -0,0 +1,16 @@
+# Adds two numbers
+#
+# descriptions
+#
+# @param x numeric first number
+# @param y numeric second number
+#
+# @return sum of values
+#
+#
+
+def adding_numbers(x,y):
+    z = x+y
+    print(z)
+    return(z)
+
