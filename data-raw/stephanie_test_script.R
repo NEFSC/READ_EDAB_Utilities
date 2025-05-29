93b28dd7c59655d23020de113bde5a205450d7f1
Stephanie Owen <stephanie.owen@noaa.gov>
2025-05-29 12:08:30 -0500

    fix raster reading in convert_longitude

--- data-raw/stephanie_test_script.R
+++ data-raw/stephanie_test_script.R
@@ -1,15 +1,14 @@
 # stephanie test script
 
-pak::pak("https://github.com/NEFSC/READ_EDAB_Utilities")
+# library(dplyr)
+# pak::pak("https://github.com/NEFSC/READ_EDAB_Utilities")
 # Install error: Error in rename(., lat = "LAT", lon = "LON", date = "EST_TOWDATE") : 
                 #could not find function "rename"
 #find_files("rename") - commented out lines of code in "extract_daily_coord.R" where "rename(., lat = "LAT", lon = "LON", date = "EST_TOWDATE")" is
+devtools::load_all()
 
 convert_longitude(data = here::here('data/sst_test.nc'))
-convert_longitude(data = here::here('data/GLORYS_daily_BottomDalinity_2019.nc'))
-# Error for both inputs: 
-#Error: unable to find an inherited method for function ‘ext’ for signature ‘x = "character"’"
-
+convert_longitude(data = here::here('data/GLORYS_daily_BottomSalinity_2019.nc'))
 
 
 test_glorys <- make_2d_summary_ts(data.in = here::here('data','GLORYS_daily_BottomSalinity_2019.nc'),
