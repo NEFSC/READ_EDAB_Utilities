c879ca835376ff97d9afe111d6ac0a301a242600
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2026-01-07 12:22:06 -0500

    input normalization

--- R/mask_nc_2d.R
+++ R/mask_nc_2d.R
@@ -2,79 +2,110 @@
 #'
 #' descriptions
 #'
-#' @param data.in Either a character vector of full input file names for a list of spatRasters
-#' @param output.files character vector of full output file names corresponding to each input file
-#' @param shp.file  string. Shape file you wish to crop each input file to
+#' @param data.in Either a character vector of full input file names, a list of SpatRasters, or a single SpatRaster (with one or multiple layers)
+#' @param output.files character vector of full output file names corresponding to each input item. Required if write.out is TRUE.
+#' @param shp.file  string, SpatVector, or SpatRaster. Shape file you wish to crop each input file to
 #' @param var.name string. Variable name you wish to extract 
 #' @param write.out logical. If TRUE, will write a netCDF file with output.files. If FALSE will return a list of spatRasters
 #' @param min.value numeric. Minimum value of var.name 
 #' @param max.value numeric. maximum value of var.name
 #' @param binary logical. Whether mask should be binary (0 for outside range, 1 for inside range) or the original values
+#' @param area.names character vector. specific area names to filter the shapefile
 #'
-#' @return netCDF file with same time dimensions as input file 
+#' @return A list of SpatRasters (if write.out=F)
 #' 
 #' @export
 
-mask_nc_2d <- function(data.in,write.out = F,output.files,shp.file =NA,var.name,min.value,max.value,binary = F,area.names =NA){
+mask_nc_2d <- function(data.in, write.out = F, output.files = NULL, shp.file = NA, var.name, min.value, max.value, binary = F, area.names = NA){
   
-  if(class(shp.file) %in% c('SpatVector','SpatRaster')){
+  # --- Refactor Step 1: Normalize Input to List ---
+  # This handles the user's requested 3 cases:
+  # 1) Vector of files -> converted to list of files
+  # 2) Single SpatRaster (multi-layer) -> converted to list of 1 SpatRaster (processed as a block)
+  # 3) List of SpatRasters -> kept as list
+  
+  if (inherits(data.in, "SpatRaster")) {
+    data.ls = list(data.in)
+  } else if (is.character(data.in)) {
+    data.ls = as.list(data.in)
+  } else if (is.list(data.in)) {
+    data.ls = data.in
+  } else {
+    stop("data.in must be a vector of file names, a list of SpatRasters, or a single SpatRaster.")
+  }
+  
+  # Check output files alignment if writing
+  if(write.out){
+    if(is.null(output.files) || length(output.files) != length(data.ls)){
+      stop("If write.out is TRUE, output.files must be provided and match the number of input items (1 output file per input object/file).")
+    }
+  }
+  
+  # --- Shapefile Handling ---
+  if(inherits(shp.file, c('SpatVector','SpatRaster'))){
     shp.vect = shp.file
-    use.shp =T
-  }else if(!is.na(shp.file)){
+    use.shp = T
+  }else if(is.character(shp.file) && !is.na(shp.file)){
     shp.vect = terra::vect(shp.file)
-    use.shp =T
+    use.shp = T
   }else{
     use.shp = F
   }
   
-  if(all(!is.na(area.names))){
+  # Filter shapefile by area names
+  if(use.shp && all(!is.na(area.names))){
     shp.str = as.data.frame(shp.vect)
-    which.att = which(apply(shp.str,2,function(x) all(area.names %in% x)))
-    which.area =  match(area.names,shp.str[,which.att])
-    shp.vect = shp.vect[which.area]  
+    # Find column containing all area.names (robust check)
+    which.att = which(apply(shp.str, 2, function(x) all(area.names %in% x)))
+    
+    if(length(which.att) > 0){
+      which.area = match(area.names, shp.str[,which.att[1]])
+      shp.vect = shp.vect[which.area]  
+    }
   }
   
   out.ls = list()
-  for(i in 1:length(data.in)){
+  
+  # --- Process Data ---
+  for(i in 1:length(data.ls)){
     
-    if(is.character(data.in)){
-      
-      data = terra::rast(data.in[i])
-      
-    }else if(class(data.in[[i]])[1] == 'SpatRaster'){
-      
-      data = data.in[[i]]
-      
-    }else{
-      stop('data.in needs to be either file names or spatRasters')
+    item = data.ls[[i]]
+    
+    # Resolve input item to SpatRaster
+    if(is.character(item)){
+      data = terra::rast(item)
+    } else if(inherits(item, "SpatRaster")){
+      data = item
+    } else {
+      stop("Input list elements must be file paths or SpatRaster objects.")
     } 
     
+    # Crop and Mask
     if(use.shp){
-      data = terra::crop(data,shp.vect)
-      data = terra::mask(data,shp.vect) 
+      data = terra::crop(data, shp.vect)
+      data = terra::mask(data, shp.vect) 
     }
     
-    data.zero.mask = terra::subset(data,1)*0
-    
+    # Clamp data (Masking by value range)
+    # values=F sets outside values to NA
     data.mask = terra::clamp(data, lower = min.value, upper = max.value, values = F)
     
     if(binary){
-       data.mask = (data.mask*0)+1
-       data.out = sum(data.mask,data.zero.mask)
+      # Create binary mask (1 for valid, NA for invalid)
+      data.mask = (data.mask * 0) + 1
+      data.out = data.mask
     }else{
       data.out = data.mask
     }
-    # data.out = sum(data.mask*data.zero.mask,na.rm=T)
     
     if(write.out){
-      terra::writeCDF(data.out, output.files[i],varname = var.name,overwrite =T)
+      terra::writeCDF(data.out, output.files[i], varname = var.name, overwrite = TRUE)
     }else{
       out.ls[[i]] = data.out
     }
   }
-
-  if(write.out ==F){
+  
+  if(write.out == F){
     return(out.ls)  
   }
-  
 }
